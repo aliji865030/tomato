e4d1b0e135e5ef2bf85650c2a4b22ef151770431
aliji865030 <abbasalichand786@gmail.com>
2024-04-09 12:17:10 +0530

    some modification

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,7 +3,10 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header">
+    <div
+      className="header"
+      style={{ backgroundImage: "url('./header_img.png')" }}
+    >
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
         <p>
