1e46945f5ae1efb923e5e735d7557b6b9b65c23a
aliji865030 <abbasalichand786@gmail.com>
2024-04-12 21:52:47 +0530

    footer modification

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -53,7 +53,7 @@ const Footer = () => {
       </div>
       <hr />
       <p className="footer-copyright">
-        Copyright 2024 Tomato.com - All Right Reserved.
+        Copyright 2024 TasteTrekker.com - All Right Reserved.
       </p>
     </div>
   );
